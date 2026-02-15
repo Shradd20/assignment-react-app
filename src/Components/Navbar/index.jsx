04953c8d22653397361feba9b513d9775e5df20e
shraddha shinde <shraddhashinde73850@gmail.com>
2026-02-15 22:07:10 +0530

    route of css

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
