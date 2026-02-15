c73180cca3a0cbab484e220759b84b03a2b640f5
shraddha shinde <shraddhashinde73850@gmail.com>
2026-02-15 22:05:38 +0530

    css file for homepage

--- src/Components/Homepage/index.jsx
+++ src/Components/Homepage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './HomePage.css';
+import './homepage.css';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 
